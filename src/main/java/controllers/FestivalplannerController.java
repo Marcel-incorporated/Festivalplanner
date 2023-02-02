989b3d4a04042dfcb3ba554e835a7e2ff0ba9e25
Maersel <marcelqualm1@gmail.com>
2023-02-02 15:37:23 +0100

    initial commit

---
+++ src/main/java/controllers/FestivalplannerController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class FestivalplannerController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.festivalplanner {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens GUI to javafx.fxml;
+    exports GUI;
+    exports controllers;
+    opens controllers to javafx.fxml;
+}
