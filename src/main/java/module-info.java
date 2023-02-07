00d8e25979344f1c82b9645b1c101f363f8bceec
Marcel Qualm <58236901+Maersel@users.noreply.github.com>
2023-02-07 21:03:02 +0100

    File generator (#19)
    * added schedule graphics.
    started working on popup for adding and editing events.
    
    Signed-off-by: Maersel <marcelqualm1@gmail.com>
    
    * made good progress on file generator.
    
    Signed-off-by: Maersel <marcelqualm1@gmail.com>
    
    ---------
    
    Signed-off-by: Maersel <marcelqualm1@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.festivalplanner {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires java.logging;
 
     opens GUI to javafx.fxml;
     exports GUI;
