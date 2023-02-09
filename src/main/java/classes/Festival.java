f66775fd412be19ceea7033a28f2b8a9cabc2174
GertieMeneer <101462876+GertieMeneer@users.noreply.github.com>
2023-02-09 19:33:24 +0100

    fixed everything (#65)
    * changed some FXML code
    
    * Fixed last bug thing
    
    * fixed everything
    
    ---------
    
    Co-authored-by: Stef <s.kraaijeveld3@student.avans.nl>
    Co-authored-by: Stef Kraaijeveld <117903018+Stefsk-glitch@users.noreply.github.com>

--- src/main/java/classes/Festival.java
+++ src/main/java/classes/Festival.java
@@ -1,13 +1,12 @@
 package classes;
 
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Festival implements Serializable {
     private ArrayList<Visitor> visitorList;
     private String name;
-    private ArrayList<Artist> artists;
+    private ArrayList<Artist> artists = new ArrayList<>();
 
     public Festival(ArrayList<Visitor> visitors, String name, ArrayList<Artist> artists) {
         this.visitorList = visitors;
