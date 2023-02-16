e1f0f6413cb07690e1cde1ad96c7a3741ad147b0
Marcel Qualm <58236901+Maersel@users.noreply.github.com>
2023-02-16 21:20:02 +0100

    Fxmlandcontrollerpatch (#91)
    * Made a seperate FXML file and controller class for each tab.
    
    ---------
    
    Signed-off-by: Maersel <marcelqualm1@gmail.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.example.festivalplanner {
     requires java.desktop;
     requires java.logging;
     requires datafaker;
-    requires fxgraphics2d;
+    requires org.jfree.fxgraphics2d;
 
     opens GUI to javafx.fxml;
     exports GUI;
