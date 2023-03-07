143cd7727f3f7f41d643d157239ba51c61e2c3d6
Sohype302 <sohaib302@outlook.com>
2023-03-07 12:37:17 +0100

    made map in sim tab

--- src/main/java/controllers/SimulatorController.java
+++ src/main/java/controllers/SimulatorController.java
@@ -1,4 +1,44 @@
 package controllers;
 
+import classes.Map;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import org.jfree.fx.FXGraphics2D;
+
+import java.awt.*;
+import java.io.FileNotFoundException;
+
 public class SimulatorController {
+
+    @FXML
+    public Canvas simMap;
+
+    private Map map;
+
+    @FXML
+    public void initialize() throws FileNotFoundException {
+        map = new Map("map.json");
+
+        FXGraphics2D g2d = new FXGraphics2D(simMap.getGraphicsContext2D());
+        new AnimationTimer() {
+            long last = -1;
+            @Override
+            public void handle(long now) {
+                if(last == -1)
+                    last = now;
+                update((now - last) / 1000000000.0);
+                last = now;
+                draw(g2d);
+            }
+        }.start();
+    }
+
+    public void update(double deltaTime) {
+        System.out.println("update sim");
+    }
+
+    public void draw(Graphics2D g) {
+        map.draw(g);
+    }
 }
