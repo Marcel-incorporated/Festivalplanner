47df61ed73f302b15bf83cc2a3673bd308540c0b
GertieMeneer <gertjan.vat@icloud.com>
2023-03-09 11:45:49 +0100

    working on sim timer

--- src/main/java/controllers/SimulatorController.java
+++ src/main/java/controllers/SimulatorController.java
@@ -18,15 +18,17 @@ public class SimulatorController {
     public Canvas timerCanvas;
     @FXML
     private Label statusLabel;
+    @FXML
+    private Label timeLabel;
 
     private Map map;
     private int minutes;
     private int hours;
     private boolean run;
+    private boolean updateTime = true;
 
     @FXML
     public void initialize() throws FileNotFoundException {
-        statusLabel.setText("Status: stopped");
         map = new Map("map.json");
 
         FXGraphics2D g2d = new FXGraphics2D(simMap.getGraphicsContext2D());
@@ -35,32 +37,38 @@ public class SimulatorController {
             long last = -1;
             @Override
             public void handle(long now) {
-                while(run) {
-                    if(last == -1)
-                        last = now;
-                    update((now - last) / 1000000000.0);
+                if(last == -1)
                     last = now;
-//                    draw(g2d);
-                }
-
+                update((now - last) / 1000000000.0);
+                last = now;
             }
         }.start();
 
     }
 
     public void update(double deltaTime) {
-        System.out.println("update sim");
-        if(run)
-            statusLabel.setText("Status: started");
-        else
-            statusLabel.setText("Status: stopped");
+//        System.out.println("update sim");
+        if(run && updateTime) {
+            updateTime();
+            updateTime = false;
+        }
     }
 
     public void draw(Graphics2D g) {
         map.draw(g);
         Graphics2D timerDrawer = new FXGraphics2D(timerCanvas.getGraphicsContext2D());
-        timerDrawer.drawLine(0, 0, 100, 100);
 
+    }
+
+    private void updateTime() {
+        if(run) {
+            statusLabel.setText("Status: started");
+            timeLabel.setText("" + hours + ":" + minutes);
+        }
+        else {
+            statusLabel.setText("Status: stopped");
+
+        }
     }
 
     @FXML
